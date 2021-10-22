00b5e0215ae39ccd239a07a2412ed178525389ef
Vijay P V <pvvijayraj1998@gmail.com>
2021-10-22 12:07:34 +0530

    Final commit

---
+++ src/test/java/com/vijay/commandlinerunner/comandlinerunner/ComandLineRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.commandlinerunner.comandlinerunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandLineRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
